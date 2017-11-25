466af557c3fb5adac7ba5149289cd88f6e0a8663
wh_all4you <wh_all4you@hotmail.com>
2017-11-25 12:40:18 +0800

    add startup args support

--- src/main/java/com/redant/cluster/slave/SlaveServerBootstrap.java
+++ src/main/java/com/redant/cluster/slave/SlaveServerBootstrap.java
@@ -2,6 +2,7 @@ package com.redant.cluster.slave;
 
 import com.redant.cluster.service.register.Registery;
 import com.redant.core.ServerInitUtil;
+import com.xiaoleilu.hutool.util.NumberUtil;
 
 /**
  * SlaveServerBootstrap
@@ -12,15 +13,22 @@ public class SlaveServerBootstrap {
 
     public static void main(String[] args) {
 
+        SlaveNode slaveNode;
+        if(args.length==1 && NumberUtil.isInteger(args[0])){
+            slaveNode = new SlaveNode(Integer.parseInt(args[0]));
+        }else{
+            slaveNode = SlaveNode.DEFAULT_PORT_NODE;
+        }
+
         // 注册Slave到ZK
-        Registery.register(SlaveNode.DEFAULT_PORT_NODE);
+        Registery.register(slaveNode);
 
         // 各种初始化工作
         ServerInitUtil.init();
 
         // 启动SlaveServer
         SlaveServer slaveServer = new SlaveServer();
-        slaveServer.start(SlaveNode.DEFAULT_PORT_NODE);
+        slaveServer.start(slaveNode);
 
     }
 }
