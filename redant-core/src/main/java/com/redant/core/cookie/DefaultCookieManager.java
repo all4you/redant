805907751352134e3dd95a00b52999915f4b9d39
all4you <wh_all4you@hotmail.com>
2019-01-16 17:34:17 +0800

    优化handler的流程，重写CookieManager

---
+++ redant-core/src/main/java/com/redant/core/cookie/DefaultCookieManager.java
@@ -0,0 +1,140 @@
+package com.redant.core.cookie;
+
+import cn.hutool.core.util.StrUtil;
+import com.redant.core.TemporaryDataHolder;
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.DefaultCookie;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Cookie管理器
+ * @author houyi.wh
+ * @date 2019-01-16
+ */
+
+public class DefaultCookieManager implements CookieManager {
+
+    /**
+     * CookieManager的实例(单例)
+     */
+    private static CookieManager cookieManager;
+
+    private DefaultCookieManager() {
+
+    }
+
+    public static CookieManager getInstance(){
+        if(cookieManager ==null) {
+            synchronized (DefaultCookieManager.class) {
+                if(cookieManager ==null) {
+                    cookieManager = new DefaultCookieManager();
+                }
+            }
+        }
+        return cookieManager;
+    }
+
+
+    @Override
+    public Set<Cookie> getCookies() {
+        HttpRequest request = TemporaryDataHolder.loadHttpRequest();
+        Set<Cookie> cookies = new HashSet<>();
+        if(request != null) {
+            String value = request.headers().get(HttpHeaderNames.COOKIE);
+            if (value != null) {
+                cookies = ServerCookieDecoder.STRICT.decode(value);
+            }
+        }
+        return cookies;
+    }
+
+    @Override
+    public Map<String, Cookie> getCookieMap() {
+        Map<String,Cookie> cookieMap = new HashMap<>();
+        Set<Cookie> cookies = getCookies();
+        if(null!=cookies && !cookies.isEmpty()){
+            for(Cookie cookie : cookies){
+                cookieMap.put(cookie.name(), cookie);
+            }
+        }
+        return cookieMap;
+    }
+
+    @Override
+    public Cookie getCookie(String name) {
+        Map<String,Cookie> cookieMap = getCookieMap();
+        return cookieMap.getOrDefault(name, null);
+    }
+
+    @Override
+    public String getCookieValue(String name) {
+        Cookie cookie = getCookie(name);
+        return cookie==null?null:cookie.value();
+    }
+
+    @Override
+    public void setCookie(Cookie cookie) {
+        TemporaryDataHolder.storeCookie(cookie);
+    }
+
+    @Override
+    public void setCookies() {
+        Set<Cookie> cookies = getCookies();
+        if (!cookies.isEmpty()) {
+            for (Cookie cookie : cookies) {
+                setCookie(cookie);
+            }
+        }
+    }
+
+    @Override
+    public void addCookie(String name, String value) {
+        addCookie(name,value,null);
+    }
+
+    @Override
+    public void addCookie(String name, String value, String domain) {
+        addCookie(name,value,domain,0);
+    }
+
+    @Override
+    public void addCookie(String name, String value, long maxAge) {
+        addCookie(name,value,null,maxAge);
+    }
+
+    @Override
+    public void addCookie(String name, String value, String domain, long maxAge) {
+        if(StrUtil.isNotBlank(name) && StrUtil.isNotBlank(value)) {
+            Cookie cookie = new DefaultCookie(name, value);
+            cookie.setPath("/");
+            if (domain != null && domain.trim().length() > 0) {
+                cookie.setDomain(domain);
+            }
+            if (maxAge > 0) {
+                cookie.setMaxAge(maxAge);
+            }
+            setCookie(cookie);
+        }
+    }
+
+    @Override
+    public boolean deleteCookie(String name) {
+        Cookie cookie = getCookie(name);
+        if(cookie!=null){
+            cookie.setMaxAge(0);
+            cookie.setPath("/");
+            setCookie(cookie);
+            return true;
+        }
+        return false;
+    }
+
+
+}
